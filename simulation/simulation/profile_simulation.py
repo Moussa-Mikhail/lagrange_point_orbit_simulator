9893e6c27ba73343e8807ae958dc70665ec4897c
Moussa Mikhail <moussamikhail56@gmail.com>
2022-08-11 22:07:28 -0500

    change to call method directly

--- simulation/simulation/profile_simulation.py
+++ simulation/simulation/profile_simulation.py
@@ -2,11 +2,11 @@ import pstats
 
 import cProfile
 
-import simulation
+from simulation import Simulation
 
-_ = simulation.simulate()
+sim = Simulation()
 
-cProfile.runctx("simulation.main()", globals(), locals(), "Profile.prof")
+cProfile.runctx("sim.simulate()", globals(), locals(), "Profile.prof")
 
 s = pstats.Stats("Profile.prof")
 s.strip_dirs().sort_stats("cumtime").print_stats(20)
