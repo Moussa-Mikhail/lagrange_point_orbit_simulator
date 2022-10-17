7e653ade60f237800cce1ac90b43bf11d0b1fb69
Moussa Mikhail <moussamikhail56@gmail.com>
2022-10-16 20:33:37 -0500

    change module docstring

--- simulation/simulation/sim_types.py
+++ simulation/simulation/sim_types.py
@@ -1,5 +1,5 @@
 """"This module contains type aliases for NDArray[np.double] which are used
-to distinguish between different 1 and 2 dimensional arrays."""
+to distinguish between 1 and 2 dimensional arrays."""
 from numpy import double
 from numpy.typing import NDArray
 
