b74c979fe381e92506631307267d195e5dcb80ca
Moussa Mikhail <moussamikhail56@gmail.com>
2022-10-18 19:02:19 -0500

    remove "Without reallocation" section

--- simulation/simulator/perf_test.py
+++ simulation/simulator/perf_test.py
@@ -13,15 +13,3 @@ for _ in range(num):
 end = time.perf_counter()
 
 print(f"Without reallocation: {(end - start)/num} seconds per simulation")
-
-start = time.perf_counter()
-
-for _ in range(num):
-
-    sim._allocate_arrays()
-
-    sim.simulate()
-
-end = time.perf_counter()
-
-print(f"With reallocation: {(end - start)/num} seconds per simulation")
