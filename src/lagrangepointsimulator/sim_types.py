43b38c6b3d2d9d7667b2c6d0ff2d9658ebdcddf0
Moussa Mikhail <moussamikhail56@gmail.com>
2023-05-14 12:54:56 -0500

    add TypeAlias

--- src/lagrangepointsimulator/sim_types.py
+++ src/lagrangepointsimulator/sim_types.py
@@ -1,9 +1,10 @@
 """This module contains type aliases for NDArray[np.double] which are used
 to distinguish between 1 and 2-dimensional arrays."""
-# TODO: move this to simulator.py and annotate.
+from typing import TypeAlias
+
 from numpy import double
 from numpy.typing import NDArray
 
-Array1D = NDArray[double]
+Array1D: TypeAlias = NDArray[double]
 
-Array2D = NDArray[double]
+Array2D: TypeAlias = NDArray[double]
