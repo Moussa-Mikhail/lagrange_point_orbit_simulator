873bdcb72805ef041a07905bec745e4f543e0f06
Moussa Mikhail <moussamikhail56@gmail.com>
2023-05-29 23:05:22 -0500

    optimize and refactor

--- src/lagrangepointsimulator/numba_funcs.py
+++ src/lagrangepointsimulator/numba_funcs.py
@@ -10,46 +10,46 @@ from .sim_types import Array1D, Array2D
 
 
 @njit(cache=True)
-def norm(vector: Array1D) -> float:
-    return sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2])
+def inverse_norm_cubed(vector: Array1D) -> float:
+    return sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]) ** -3
 
 
-# pylint: disable=too-many-arguments
+# pylint: disable=too-many-arguments, too-many-locals
 @njit(cache=True)
 def calc_acceleration(
-    star_mass: float,
-    planet_mass: float,
+    G_star: float,
+    G_planet: float,
     star_pos: Array1D,
     planet_pos: Array1D,
     sat_pos: Array1D,
     star_accel: Array1D,
     planet_accel: Array1D,
     sat_accel: Array1D,
-    r_planet_to_star: Array1D,
-    r_sat_to_star: Array1D,
-    r_sat_to_planet: Array1D,
+    planet_to_star: Array1D,
+    sat_to_star: Array1D,
+    sat_to_planet: Array1D,
 ) -> None:
     for j in range(3):
-        # vector from planet to star
-        r_planet_to_star[j] = star_pos[j] - planet_pos[j]
-        r_sat_to_star[j] = star_pos[j] - sat_pos[j]
-        r_sat_to_planet[j] = planet_pos[j] - sat_pos[j]
+        planet_to_star[j] = star_pos[j] - planet_pos[j]
+        sat_to_star[j] = star_pos[j] - sat_pos[j]
+        sat_to_planet[j] = planet_pos[j] - sat_pos[j]
 
-    # distance between star to planet
-    d_planet_to_star = norm(r_planet_to_star)
-    d_sat_to_star = norm(r_sat_to_star)
-    d_sat_to_planet = norm(r_sat_to_planet)
+    d_planet_to_star_inverse_cubed = inverse_norm_cubed(planet_to_star)
+    d_sat_to_star_inverse_cubed = inverse_norm_cubed(sat_to_star)
+    d_sat_to_planet_inverse_cubed = inverse_norm_cubed(sat_to_planet)
+
+    star_planet_coeff = G_planet * d_planet_to_star_inverse_cubed
+    planet_star_coeff = G_star * d_planet_to_star_inverse_cubed
+    sat_star_coeff = G_star * d_sat_to_star_inverse_cubed
+    sat_planet_coeff = G_planet * d_sat_to_planet_inverse_cubed
 
     for j in range(3):
-        star_accel[j] = -G * planet_mass * r_planet_to_star[j] / d_planet_to_star**3
+        star_accel[j] = -star_planet_coeff * planet_to_star[j]
 
-        # note the lack of negative sign in the following lines
-        planet_accel[j] = G * star_mass * r_planet_to_star[j] / d_planet_to_star**3
+        # note the lack of negative signs in the following lines
+        planet_accel[j] = planet_star_coeff * planet_to_star[j]
 
-        sat_accel[j] = (
-            G * star_mass * r_sat_to_star[j] / d_sat_to_star**3
-            + G * planet_mass * r_sat_to_planet[j] / d_sat_to_planet**3
-        )
+        sat_accel[j] = sat_star_coeff * sat_to_star[j] + sat_planet_coeff * sat_to_planet[j]
 
 
 # pylint: disable=too-many-arguments, too-many-locals
@@ -74,56 +74,59 @@ def integrate(
     planet_intermediate_pos = np.empty_like(star_accel)
     sat_intermediate_pos = np.empty_like(star_accel)
 
-    r_planet_to_star = np.empty_like(star_accel)
-    r_sat_to_star = np.empty_like(star_accel)
-    r_sat_to_planet = np.empty_like(star_accel)
+    planet_to_star = np.empty_like(star_accel)
+    sat_to_star = np.empty_like(star_accel)
+    sat_to_planet = np.empty_like(star_accel)
+
+    half_time_step = 0.5 * time_step
+
+    G_star = G * star_mass
+    G_planet = G * planet_mass
 
     for k in range(1, num_steps + 1):
         for j in range(3):
-            # intermediate position calculation
-            star_intermediate_pos[j] = star_pos[k - 1, j] + 0.5 * star_vel[k - 1, j] * time_step
+            star_intermediate_pos[j] = star_pos[k - 1, j] + star_vel[k - 1, j] * half_time_step
 
-            planet_intermediate_pos[j] = planet_pos[k - 1, j] + 0.5 * planet_vel[k - 1, j] * time_step
+            planet_intermediate_pos[j] = planet_pos[k - 1, j] + planet_vel[k - 1, j] * half_time_step
 
-            sat_intermediate_pos[j] = sat_pos[k - 1, j] + 0.5 * sat_vel[k - 1, j] * time_step
+            sat_intermediate_pos[j] = sat_pos[k - 1, j] + sat_vel[k - 1, j] * half_time_step
 
-        # acceleration calculation
         calc_acceleration(
-            star_mass,
-            planet_mass,
+            G_star,
+            G_planet,
             star_intermediate_pos,
             planet_intermediate_pos,
             sat_intermediate_pos,
             star_accel,
             planet_accel,
             sat_accel,
-            r_planet_to_star,
-            r_sat_to_star,
-            r_sat_to_planet,
+            planet_to_star,
+            sat_to_star,
+            sat_to_planet,
         )
 
         for j in range(3):
-            # velocity update
             star_vel[k, j] = star_vel[k - 1, j] + star_accel[j] * time_step
+
             planet_vel[k, j] = planet_vel[k - 1, j] + planet_accel[j] * time_step
+
             sat_vel[k, j] = sat_vel[k - 1, j] + sat_accel[j] * time_step
 
-            # position update
-            star_pos[k, j] = star_intermediate_pos[j] + 0.5 * star_vel[k, j] * time_step
+            star_pos[k, j] = star_intermediate_pos[j] + star_vel[k, j] * half_time_step
 
-            planet_pos[k, j] = planet_intermediate_pos[j] + 0.5 * planet_vel[k, j] * time_step
+            planet_pos[k, j] = planet_intermediate_pos[j] + planet_vel[k, j] * half_time_step
 
-            sat_pos[k, j] = sat_intermediate_pos[j] + 0.5 * sat_vel[k, j] * time_step
+            sat_pos[k, j] = sat_intermediate_pos[j] + sat_vel[k, j] * half_time_step
 
 
 @njit(parallel=True, cache=True)
 def transform_to_corotating(pos_trans: Array2D, times: Array1D, angular_speed: float) -> Array2D:
-    """Transforms pos_trans to a frame of reference that rotates at a rate of angular_speed.
+    """Transforms pos_trans to a frame of reference that rotates at a rate of angular_speed counter-clockwise.
     pos_trans is an array of positions measured relative to the center of rotation.
     """
 
     # we do this by linearly transforming each position vector by
-    # the inverse of the coordinate transform
+    # the inverse of the basis transform
     # the coordinate transform is unit(x) -> R(w*t)*unit(x), unit(y) -> R(w*t)*unit(y)
     # where R(w*t) is the rotation matrix with angle w*t about the z axis
     # the inverse is R(-w*t)
