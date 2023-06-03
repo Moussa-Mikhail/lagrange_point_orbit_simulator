59e202fa27b451891834637e79b724c2b09d476e
Moussa Mikhail <moussamikhail56@gmail.com>
2023-06-03 18:21:47 -0500

    rework comments

--- src/lagrangepointsimulator/constants.py
+++ src/lagrangepointsimulator/constants.py
@@ -4,16 +4,13 @@
 G = 6.67430 * 10**-11
 
 # 1 AU in meters
-# serves as a conversion factor from AUs to meters
 AU = 1.495978707 * 10**11
-
-# 1 Julian year in seconds
-# serves as a conversion factor from years to seconds
-YEARS = 365.25 * 24 * 60 * 60
 
 HOURS = 60 * 60
 
-# mass of Sun in kilograms
+YEARS = 365.25 * 24 * HOURS
+
+# Masses in kilograms
 SUN_MASS: float = 1.98847 * 10**30
 
 EARTH_MASS = 5.9722 * 10**24
@@ -22,7 +19,7 @@ MOON_MASS = 7.34767309 * 10**22
 
 JUPITER_MASS: float = 1.89813 * 10**27
 
-# distance between Earth and Sun in AU
+# Distances in Astronomical Units
 SUN_EARTH_DIST = 0.9954
 
 SUN_JUPITER_DIST = 4.953
