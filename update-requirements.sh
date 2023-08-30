f21e8304807a20591ca0f8deca052dcc041b48fc
Moussa Mikhail <moussamikhail56@gmail.com>
2023-08-30 00:04:13 -0500

    Revert "build(deps): remove requirements-dev.txt"
    This reverts commit e456f8821d0ecb74b64a386632b9fa01c284eeb3.

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,2 +1,3 @@
 poetry export -f requirements.txt --output requirements.txt --without-hashes
+poetry export -f requirements.txt --output requirements-dev.txt --with dev --without-hashes
 poetry export -f requirements.txt --output requirements-gui.txt -E gui --without-hashes
