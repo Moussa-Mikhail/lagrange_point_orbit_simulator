e456f8821d0ecb74b64a386632b9fa01c284eeb3
Moussa Mikhail <moussamikhail56@gmail.com>
2023-08-29 23:52:19 -0500

    build(deps): remove requirements-dev.txt

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,3 +1,2 @@
 poetry export -f requirements.txt --output requirements.txt --without-hashes
-poetry export -f requirements.txt --output requirements-dev.txt --with dev --without-hashes
 poetry export -f requirements.txt --output requirements-gui.txt -E gui --without-hashes
