df9e14a73dbe61bc8659af0e0e7bf883d5703506
Moussa Mikhail <moussamikhail56@gmail.com>
2023-08-29 23:48:25 -0500

    build(deps): change gui group to extra

--- update-requirements.sh
+++ update-requirements.sh
@@ -1,3 +1,3 @@
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 poetry export -f requirements.txt --output requirements-dev.txt --with dev --without-hashes
-poetry export -f requirements.txt --output requirements-gui.txt --with gui --without-hashes
+poetry export -f requirements.txt --output requirements-gui.txt -E gui --without-hashes
