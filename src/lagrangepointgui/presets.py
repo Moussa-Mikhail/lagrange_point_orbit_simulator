2522a235e09b933f004891c237f3cc74276931e3
Moussa Mikhail <moussamikhail56@gmail.com>
2023-10-14 14:43:48 -0500

    fix: detect if running from build on macOS
    refactor: import tomllib as tomli

--- src/lagrangepointgui/presets.py
+++ src/lagrangepointgui/presets.py
@@ -3,17 +3,19 @@
 from typing import TypeAlias
 
 try:
-    import tomllib  # type: ignore
+    import tomllib as tomli  # type: ignore
 except ModuleNotFoundError:
-    import tomli as tomllib
+    import tomli
 
 import os
+import sys
 from pathlib import Path
 
 dir_path = Path(os.path.realpath(__file__)).parent
-# if running from app set dir_path to the "sim_gui" directory
-# otherwise keep it as the "lagrangepointgui" directory
-if "sim_gui" in dir_path.parts:
+
+# if running from app set dir_path to the _internal directory
+# otherwise keep it as the directory of this module
+if getattr(sys, "frozen", False):
     dir_path = dir_path.parent.parent
 
 default_presets_path = dir_path / "default_presets.toml"
@@ -36,7 +38,7 @@ def read_presets() -> tuple[ParamPresets, Constants]:
 def _read_preset(file_path: Path) -> tuple[ParamPresets, Constants]:
     try:
         with Path.open(file_path, "rb") as file:
-            presets = tomllib.load(file)
+            presets = tomli.load(file)
     except FileNotFoundError:
         return {}, {}
 
